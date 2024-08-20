a2bd0256379a54e06b04ea8564f99a9a7475fae8
mariajose <mariamariapava123@gmail.com>
2024-08-19 21:44:07 -0500

    Diseño de la caja de login. Creacion de la carpeta .streamlit con el archivo .toml que incluye los estilos del tema general de la app

--- interfaces/Login.py
+++ interfaces/Login.py
@@ -1,3 +1,29 @@
 import streamlit as st
 
-st.text_input("Enter password:", key="password")
+
+# Create the form
+with st.form("My form"):
+    st.markdown("### JavePOS")  
+
+    first = st.text_input("User", key="user")
+    last = st.text_input("Password", key="password", type="password")
+    
+    submit = st.form_submit_button("Submit")
+
+   
+
+# Apply additional custom CSS for the form
+css = """
+<style>
+    [data-testid="stForm"] {
+        background: #10B981;
+        padding: 20px;
+        border-radius: 30px;
+    }
+    [data-testid="stForm"] h3 {
+        text-align: center;
+    }
+</style>
+"""
+st.write(css, unsafe_allow_html=True)
+
