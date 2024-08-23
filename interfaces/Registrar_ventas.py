5bd88dbf5d34575e70d1f9ddada903d090822f41
TwitchViejoSarmi <alesansarive@gmail.com>
2024-08-23 15:47:34 -0500

    MOD: Ajustado para mostrar y filtrar los productos.

--- interfaces/Registrar_ventas.py
+++ interfaces/Registrar_ventas.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from connection import connect
 tab1, tab2, tab3,tab4 = st.tabs(["Till", "Sales", "Reports","Settings"])
 
 with tab1:
@@ -11,22 +11,32 @@ with tab1:
             with st.form("stock-add-form"):
                 col2, col3 = st.columns([3,1])
                 with col2:
-                    prompt_add = st.text_input("Hello",value="",placeholder="Enter Stock Code",label_visibility="collapsed")
+                    prompt_id = st.text_input("Hello",value="",placeholder="Enter Stock Code",label_visibility="collapsed") # Buscar por ID.
                 with col3:
-                    submitted = st.form_submit_button("Add")
+                    submitid = st.form_submit_button("Find")
         
         with col2_search:
-             with st.form("stock-search-form"):
+            with st.form("stock-search-form"):
                 col2, col3 = st.columns([3,1])
                 with col2:
-                    prompt_add = st.text_input("Hello",value="",placeholder="Enter Stock Code",label_visibility="collapsed")
+                    prompt_name = st.text_input("Hello",value="",placeholder="Enter Stock Name",label_visibility="collapsed") # Buscar por nombre.
                 with col3:
-                    submitted = st.form_submit_button("Find")
+                    submitname = st.form_submit_button("Find")
 
 
 
     with st.container():
-        st.write("Aquí se verá el dataframe con los datos de los productos")
+        refresh = st.button("Refresh")
+        if refresh or submitid or submitname: # Si el usuario refresca la info o inicia una búsqueda con filtro.
+            if submitid and prompt_id != "":
+                data = connect.query(f"SELECT * FROM productos WHERE id={prompt_id}")
+                st.dataframe(data)
+            elif submitname and prompt_name != "":
+                data = connect.query(f"SELECT * FROM productos WHERE nombre LIKE '%{prompt_name}%'")
+                st.dataframe(data)
+            else:
+                data = connect.query("SELECT * FROM productos") # Cargar todos los productos.
+                st.dataframe(data)
 
 with tab2:
     st.header("Sales")
