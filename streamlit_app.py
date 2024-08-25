9ec53f60477af769c0d3e957b5eb7092f1e26976
Jebuser <juanestebanbecerragutierrez@gmail.com>
2024-08-24 19:42:39 -0500

    MOD: Iniciar la parte de la autenticación. Agregado Menu de autenticacion con rol funcional.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,61 @@
 import streamlit as st
 
+if "role" not in st.session_state:
+    st.session_state.role = None
 
-pg = st.navigation([
-    st.Page("interfaces/Historial.py", title="Historial de Ventas"),
-    st.Page("interfaces/Registrar_ventas.py", title="Registrar Venta"),
-    st.Page("interfaces/Login.py", title="Login")
-])
+ROLES = [None, "Admin", "Empleado"]
 
-st.set_page_config(page_title="JavePOS", page_icon=":material/edit:")
+
+def login():
+
+    st.header("Bienvenido")
+    role = st.selectbox("Selecciona tu rol", ROLES)
+
+    if st.button("Iniciar sesión"):
+        st.session_state.role = role
+        st.rerun()
+
+
+def logout():
+    st.session_state.role = None
+    st.rerun()
+
+role = st.session_state.role
+
+logout_page = st.Page(logout, title="Salir", icon=":material/logout:")
+settings = st.Page("settings.py", title="Settings", icon=":material/settings:")
+Menu = st.Page(
+    "interfaces/Menu.py",
+    title="Menu",
+    icon=":material/help:",
+    default=True,
+)
+historial = st.Page(
+    "interfaces/Historial.py",
+    title="Historial de ventas",
+    icon=":material/help:",
+)
+registrar_ventas = st.Page(
+    "interfaces/Registrar_ventas.py", 
+    title="Registro de ventas", 
+    icon=":material/bug_report:",
+)
+
+account_pages = [logout_page, settings]
+empleado_pages = [Menu, historial, registrar_ventas]
+admin_pages = []
+
+st.title("POS Javeriana")
+
+page_dict = {}
+if st.session_state.role in ["Empleado", "Admin"]:
+    page_dict["Empleado"] = empleado_pages
+if st.session_state.role == "Admin":
+    page_dict["Admin"] = admin_pages
+
+if len(page_dict) > 0:
+    pg = st.navigation({"Account": account_pages} | page_dict)
+else:
+    pg = st.navigation([st.Page(login)])
 
 pg.run()
