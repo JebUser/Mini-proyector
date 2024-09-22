66d928b4bab68ac6ed51fd412269ef0c5c37730b
TwitchViejoSarmi <alesansarive@gmail.com>
2024-09-22 02:44:15 -0500

    MOD: Agregadas y configuradas las graficas faltantes + calculo de intervalo.

--- controller/ControladorPDF.py
+++ controller/ControladorPDF.py
@@ -1,20 +1,26 @@
 import os
 
 from fpdf import FPDF
+import pandas as pd
+from dateutil.relativedelta import relativedelta
 
 class ControladorPdf(FPDF):
 
     def generar_recibo(self):
         return
 
-    # TODO: Agregar las graficas.
-    def generar_reporte(self, total_revenue:float, last_month:int, this_month:int, per_trans:int, most_sold:str):
+    def generar_reporte(self, total_revenue:float, last_month:int, this_month:int, per_trans:int, taxes:float, diagram_path:str, circle_path:str):
         self.add_page(orientation='L')
 
         # Titulo del doc.
         self.set_xy(107,10)
         self.set_font('Arial', 'B', 24)
         self.cell(80, 10, "JAVE POS-SALES REPORT", 0, 1, 'C')
+
+        # Intervalo de cálculo.
+        self.set_xy(115, 17)
+        self.set_font('Arial', '', 12)
+        self.cell(80, 10, f'(From {(pd.Timestamp.now() - relativedelta(years=1)).strftime('%d %b %Y')} to {pd.Timestamp.now().strftime('%d %b %Y')})')
 
         # TOTAL REVENUE
         # sombra.
@@ -35,7 +41,7 @@ class ControladorPdf(FPDF):
         self.set_xy(50,55)
         self.set_text_color(0,102,153)
         self.set_font('Arial', '', 22)
-        self.cell(80, 10, f'${total_revenue}', 0, 1, 'C')
+        self.cell(80, 10, f'${total_revenue:,.2f}', 0, 1, 'C')
 
         # PRODUCTS SOLD
         # sombra.
@@ -94,13 +100,16 @@ class ControladorPdf(FPDF):
         # Titulo.
         self.set_xy(165,78)
         self.set_font('Arial', 'B', 20)
-        self.cell(80, 10, "Most Sold Product", 0, 1, 'C')
+        self.cell(80, 10, "Total Taxes", 0, 1, 'C')
 
         # Valor.
         self.set_xy(165,98)
         self.set_font('Arial', '', 22)
-        self.cell(80, 10, most_sold, 0, 1, 'C')
+        self.set_text_color(255,45,45)
+        self.cell(80, 10, f'${taxes:,.2f}', 0, 1, 'C')
 
-pdf = ControladorPdf()
-pdf.generar_reporte(38981496, 92, 50, 49, "Banana")
-pdf.output('prueba.pdf')
+        # Diagrama de lineas.
+        self.image(diagram_path, x=140, y=120, w=130)
+
+        # Diagrama Circular.
+        self.image(circle_path, x=42, y=120, w=90)
